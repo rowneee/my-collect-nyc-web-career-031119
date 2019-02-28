f0521399da02ee2f80c112a92ad5a73468091ad0
Roni Shabo <roni.shabo@gmail.com>
2019-02-28 01:58:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
-def my_collect(collection)
+def my_collect(array)
   i = 0
   new_collection = []
-  while i < collection.length  
-    new_collection.push yield(collection[i])
+  while i < array.length  
+    new_collection.push yield(array[i])
     i += 1
   end
   new_collection
