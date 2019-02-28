61bf012a34406a4688d3867166662e5bd5f5e3bb
Roni Shabo <roni.shabo@gmail.com>
2019-02-28 01:33:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length  
+    yield (collection[i])
+    i += 1
+  end
+  collection
+end
 
-
+my_collect(collection) do |element| 
+  if element.find(" ") 
+    puts element.upcase
+  elsif element.find(" ")
+    
+  
